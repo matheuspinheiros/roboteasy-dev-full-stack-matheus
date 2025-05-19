708b11e438826515e7cdb29c7ade5674c39732d4
matheuspinheiros <mathp237@gmail.com>
2025-05-19 19:39:01 -0300

    Primeiro commit desafio full stack

---
+++ Backend/API-Desafio-de-Chat-Virtual/src/main/java/com/api_desafio_chat/API/Desafio/de/Chat/Virtual/ApiDesafioDeChatVirtualApplication.java
@@ -0,0 +1,13 @@
+package com.api_desafio_chat.API.Desafio.de.Chat.Virtual;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiDesafioDeChatVirtualApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiDesafioDeChatVirtualApplication.class, args);
+	}
+
+}
---
+++ Backend/API-Desafio-de-Chat-Virtual/src/test/java/com/api_desafio_chat/API/Desafio/de/Chat/Virtual/ApiDesafioDeChatVirtualApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api_desafio_chat.API.Desafio.de.Chat.Virtual;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiDesafioDeChatVirtualApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
